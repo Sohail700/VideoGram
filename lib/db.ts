cc8ab9fbfca658cc64b8be309addfae66369ef0d
sohail <sk22222223@gmail.com>
2025-08-23 00:04:05 +0530

    On branch master Changes to be committed: 	new file:   lib/db.ts 	new file:   types.d.ts

---
+++ lib/db.ts
@@ -0,0 +1,38 @@
+import { error } from "console";
+import mongoose from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URL!;
+
+if (!MONGODB_URL) {
+  throw new Error("please define mongo_url in .env");
+}
+
+let cached = global.mongoose
+
+if (!cached) {
+  cached = global.mongoose = {conn: null, promise: null};
+}
+
+export async function connectedToDatabase() {
+  if(cached.conn){
+    return cached.conn
+  }
+
+  if(!cached.promise){
+    const opt = {
+      bufferCommands: true,
+      maxPoolSize: 10
+    }
+    mongoose
+    .connect(MONGODB_URL, opt)
+    .then(()=> mongoose.connection)
+  }
+
+  try {
+    cached.conn = await cached.promise
+  } catch (error) {
+    cached.promise = null;
+    throw error
+  }
+  return cached.conn
+}
