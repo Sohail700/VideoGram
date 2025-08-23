512d2d1408be55ad7e29c032bdd006500cf62c47
sohail <sk22222223@gmail.com>
2025-08-23 21:57:10 +0530

    Implement authentication with database connection and add Prettier configuration

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,27 +1,30 @@
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { text } from "stream/consumers";
+import { connectedToDatabase } from "./db";
+import User from "@/models/User";
 
-export const authOption:NextAuthOptions = {
-  providers: [    
+export const authOption: NextAuthOptions = {
+  providers: [
     CredentialsProvider({
       name: "Credentials",
       credentials: {
-        email: {label: "Email", type: "text"},
-        password: {label: "Password", type: "password"}
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
       },
 
       async authorize(credentials) {
-        if(!credentials?.email && !credentials?.password){
-          throw new Error("Missing email or password!")
+        if (!credentials?.email && !credentials?.password) {
+          throw new Error("Missing email or password!");
         }
-      }
 
-      try {
-        
-      } catch (error) {
-        
-      }
-    })
-  ]
-}
+        try {
+          await connectedToDatabase();
+          const user = await User.findOne({ email: credentials.email });
+          if (!user) {
+            throw new Error("No user found with this credential");
+          }
+        } catch (error) {}
+      },
+    }),
+  ],
+};
--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,4 @@
 //chai
-import { error } from "console";
 import mongoose from "mongoose";
 
 const MONGODB_URL = process.env.MONGODB_URL!;
@@ -8,32 +7,30 @@ if (!MONGODB_URL) {
   throw new Error("please define mongo_url in .env");
 }
 
-let cached = global.mongoose
+let cached = global.mongoose;
 
 if (!cached) {
-  cached = global.mongoose = {conn: null, promise: null};
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 export async function connectedToDatabase() {
-  if(cached.conn){
-    return cached.conn
+  if (cached.conn) {
+    return cached.conn;
   }
 
-  if(!cached.promise){
+  if (!cached.promise) {
     const opt = {
       bufferCommands: true,
-      maxPoolSize: 10
-    }
-    mongoose
-    .connect(MONGODB_URL, opt)
-    .then(()=> mongoose.connection)
+      maxPoolSize: 10,
+    };
+    mongoose.connect(MONGODB_URL, opt).then(() => mongoose.connection);
   }
 
   try {
-    cached.conn = await cached.promise
+    cached.conn = await cached.promise;
   } catch (error) {
     cached.promise = null;
-    throw error
+    throw error;
   }
-  return cached.conn
+  return cached.conn;
 }
